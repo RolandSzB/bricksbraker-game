34063d65d9b225d4f0a8f78163c275ab585584f2
RolandSzB <125957801+RolandSzB@users.noreply.github.com>
2023-03-17 11:17:25 +0200

    smile-head (#1)
    * smile-head
    
    * skin+right eye
    
    * left eye
    
    * added left eye+ mouth

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 function setup() {
-    createCanvas(400,500)
+    createCanvas(400,400)
 }
 
 function draw() {
- background("#ff0000")
-} 
+ background("#ff5555")
+smileFaceRoli();
+}
+
+function smileFaceRoli(){
+     //skin
+    fill("#00ff00");
+    circle(200, 200, 100);
+ 
+    //right eye
+    fill("lightblue");
+    circle(220, 190, 15);
+    fill("#000000")
+    circle(220, 190, 3);
+   
+    //left eye
+    fill("lightblue");
+    circle(180, 190, 15);
+    fill("#000000")
+    circle(180, 190, 3);
+ 
+    //mouth  
+    noFill(); 
+    arc(200, 230, 40, 15, 0, Math.PI );
+}
