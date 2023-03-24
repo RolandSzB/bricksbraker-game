d187bf619fd394bf1ea6c0285748ba696bcf5042
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-03-24 08:51:16 +0200

    adeed x and y

--- scripts.js
+++ scripts.js
@@ -1,30 +1,30 @@
 function setup() {
-    createCanvas(400,400)
+  createCanvas(400, 400);
 }
 
 function draw() {
- background("#ff5555")
-smileFaceRoli();
+  background("#ff5555");
+  smileFaceRoli(200, 200);
 }
 
-function smileFaceRoli(){
-     //skin
-    fill("#00ff00");
-    circle(200, 200, 100);
- 
-    //right eye
-    fill("lightblue");
-    circle(220, 190, 15);
-    fill("#000000")
-    circle(220, 190, 3);
-   
-    //left eye
-    fill("lightblue");
-    circle(180, 190, 15);
-    fill("#000000")
-    circle(180, 190, 3);
- 
-    //mouth  
-    noFill(); 
-    arc(200, 230, 40, 15, 0, Math.PI );
+function smileFaceRoli(x, y) {
+  //skin
+  fill("#00ff00");
+  circle(x, y, 100);
+
+  //right eye
+  fill("lightblue");
+  circle(x + 20, y - 10, 15);
+  fill("#000000");
+  circle(x + 20, y - 10, 3);
+
+  //left eye
+  fill("lightblue");
+  circle(x - 20, y - 10, 15);
+  fill("#000000");
+  circle(x - 20, y - 10, 3);
+
+  //mouth
+  noFill();
+  arc(x, y + 30, 40, 15, 0, Math.PI);
 }
