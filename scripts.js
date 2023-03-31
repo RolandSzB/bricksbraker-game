ef60ed7aeb5b29b1b87e2089f8fee834c3807870
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2023-03-31 09:49:08 +0300

    da

--- scripts.js
+++ scripts.js
@@ -3,73 +3,66 @@ function setup() {
 }
 
 function draw() {
-
   background("#ff5555");
-  smileFaceRoli(200, 200);
-  smileFaceJano(85,200)
-  
+  smileFaceRoli();
+  //smileFaceJano(85, 200);
 }
 
-function smileFaceRoli(x, y) {
+function smileFaceRoli() {
   //skin
   fill("#00ff00");
-  circle(x, y, 100);
+  circle(200, 200, 100);
 
   //right eye
   fill("lightblue");
-  circle(x + 20, y - 10, 15);
+  circle(220, 190, 15);
   fill("#000000");
-  circle(x + 20, y - 10, 3);
+  circle(220, 190, 3);
 
   //left eye
   fill("lightblue");
-  circle(x - 20, y - 10, 15);
+  circle(180, 190, 15);
   fill("#000000");
-  circle(x - 20, y - 10, 3);
+  circle(180, 190, 3);
 
   //mouth
   noFill();
-  arc(x, y + 30, 40, 15, 0, Math.PI);
+  arc(200, 170, 40, 15, 0, Math.PI);
   background("#ff0000");
-  smileFaceJano();
-  background("#ff5555");
-  smileFaceRoli();
 }
 
-function smileFaceJano(x,y) {
-  // skin
-  fill("#fdda55");
-  circle(200, 200, 100);
-  circle(x, y, 100);
+// function smileFaceJano(x, y) {
+//   // skin
+//   fill("#fdda55");
 
-  // right eye
-  fill("#000000");
-  circle(x + 220, y - 190, 25);
-  circle(x + 220, y - 190, 25);
+//   circle(x, y, 100);
 
-  fill("ffffff");
-  circle(x + 220, y - 190, 10);
-  circle(x + 220, y - 190, 10);
+//   // right eye
+//   fill("#000000");
+//   circle(x + 20, y - 10, 25);
+//   circle(x + 20, y - 10, 25);
 
-  // left eye
-  fill("ffffff");
-  circle(x + 180, y - 190, 25);
-  circle(x + 180, y - 190, 25);
+//   fill("ffffff");
+//   circle(x + 20, y - 10, 10);
+//   circle(x + 20, y - 10, 10);
 
-  fill("#000000");
-  circle(x + 180, y - 190, 10);
-  circle(x + 180, y - 190, 10);
+//   // left eye
+//   fill("ffffff");
+//   circle(x - 20, y - 10, 25);
+//   circle(x - 20, y - 10, 25);
 
-  // mouth
-  fill("#000000");
-  arc(x + 200, y - 210, 30, 30, 0, Math.PI);
+//   fill("#000000");
+//   circle(x - 20, y - 10, 10);
+//   circle(x - 20, y - 10, 10);
 
-  fill("#ff0000");
-  arc(x + 200, y - 220, 15, 10, 0, Math.PI);
+//   // mouth
+//   fill("#000000");
+//   arc(x, y + 10, 30, 30, 0, Math.PI);
 
-  //
-  fill("#fdda55");
-  arc(x + 200, y - 140, 55, 10, Math.PI, 0);
-}
+//   fill("#ff0000");
+//   arc(x, y + 20, 15, 10, 0, Math.PI);
 
-
+//   //
+//   fill("#fdda55");
+//   arc(x, y - 60, 55, 10, Math.PI, 0);
+// }
